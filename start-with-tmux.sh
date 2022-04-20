ce3dbd2fc9cf8ca55fd1259ec84a397e1278e329
balazs.saros <balazs.saros@gmail.com>
2022-04-20 17:40:49 +0200

    init keystone backend with a basic schema

--- start-with-tmux.sh
+++ start-with-tmux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-tmux send-keys 'cd ./konyha-backend && npm run develop' C-m \; \
+tmux send-keys 'cd ./keystone-backend && npm run dev' C-m \; \
   split-window -v \; \
   send-keys 'cd ./konyha-ui && npm start' C-m \; 
