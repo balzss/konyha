34d8911b63851c0a7f3eb669b6fb681359a72db0
balazs.saros <balazs.saros@gmail.com>
2022-04-22 15:17:47 +0200

    use nextlink in BrandHero

--- next-frontend/components/BrandHero.tsx
+++ next-frontend/components/BrandHero.tsx
@@ -2,11 +2,11 @@ import {
   Link,
   Typography,
 } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
+import NextLink from 'next/link';
 
 export default function BrandHero() {
   return (
-    <Link to="/" underline="none" component={RouterLink as any} sx={{color: 'inherit'}}>
+    <Link to="/" underline="none" component={NextLink as any} sx={{color: 'inherit'}}>
       <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap', paddingInline: '16px'}}>
         <img src="/logo128.png" alt="" style={{width: '80px'}}/>
         <Typography variant="h4" component="div" sx={{minWidth: '300px', textAlign: 'center'}}>
