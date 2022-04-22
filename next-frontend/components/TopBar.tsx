ae0899fd03385f9c152f7521d216b7a46032d338
balazs.saros <balazs.saros@gmail.com>
2022-04-22 15:18:03 +0200

    add key to topbar items

--- next-frontend/components/TopBar.tsx
+++ next-frontend/components/TopBar.tsx
@@ -107,7 +107,7 @@ export default function TopBar({
             sx={{zIndex: 2000}}
           >
             { hiddenActions.map(({icon, label, action}) => (
-              <MenuItem onClick={(e) => { handleCloseMenu(); action(e); }}>
+              <MenuItem key={label} onClick={(e) => { handleCloseMenu(); action(e); }}>
                 <ListItemIcon>
                   {icon}
                 </ListItemIcon>
