f0c0eebd680c2a17fb8765231c5edafbc7b6bb0d
balazs.saros <balazs.saros@gmail.com>
2022-04-25 13:09:20 +0200

    remove old backend and frontend

--- konyha-ui/src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as MainPage } from './MainPage';
-export { default as UserPage } from './UserPage';
-export { default as EditRecipePage } from './EditRecipePage';
-export { default as RecipeDetailsPage } from './RecipeDetailsPage';
-export { default as LoginPage } from './LoginPage';
-export { default as SignUpPage } from './SignUpPage';
