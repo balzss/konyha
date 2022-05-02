00fecbb27b534f9c8c70f19d5f9f31c26a06a6df
balazs.saros <balazs.saros@gmail.com>
2022-05-02 23:01:14 +0200

    export components from root

---
+++ next-frontend/components/index.tsx
@@ -0,0 +1,7 @@
+export { default as BottomNav } from './BottomNav';
+export { default as BrandHero } from './BrandHero';
+export { default as ConfirmModal } from './ConfirmModal';
+export { default as Layout } from './Layout';
+export { default as Link } from './Link';
+export { default as RecipeCard } from './RecipeCard';
+export { default as TopBar } from './TopBar';
