9d595ef540a925f751b07a576dc93eab4f8f18d8
balazs.saros <balazs.saros@gmail.com>
2022-05-16 17:31:44 +0200

    add document template

--- components/Head.tsx
+++ components/Head.tsx
@@ -10,9 +10,6 @@ export default function Head({
   return (
     <NextHead>
       <title>{title}</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta name="mobile-web-app-capable" content="yes"/>
-      <link rel="icon" type="image/png" href="/logo64.png" />
     </NextHead>
   )
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+const MyDocument = () => {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="mobile-web-app-capable" content="yes"/>
+        <link rel="icon" type="image/png" href="/logo64.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default MyDocument;
