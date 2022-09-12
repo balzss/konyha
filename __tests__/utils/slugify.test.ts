70137a4204c8764848c7773284fae49978903433
balazs.saros <balazs.saros@gmail.com>
2022-09-12 10:56:42 +0200

    fix lint error

--- __tests__/utils/slugify.test.ts
+++ __tests__/utils/slugify.test.ts
@@ -28,6 +28,6 @@ describe('utils/slugify', () => {
 
   it('removes non url characters', () => {
     const slug = slugify('+h.e?l"l(o#');
-    expect(slug).toEqual('hello')
+    expect(slug).toEqual('hello');
   });
 });
